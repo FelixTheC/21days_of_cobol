5ab047ed811eb75931698ed31bd532622ab38226
Unknown <felixeisenmenger@gmx.net>
2019-06-15 18:24:53 +0200

    [CREATE] day 1 from 21

---
+++ hello.cob
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author: Felix Eisenmenger
+      * Date:
+      * Purpose: Training
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+            DISPLAY "Hello, world!".
+       PROGRAM-DONE.
+            STOP RUN.
---
+++ minimum.cbl
@@ -0,0 +1,19 @@
+      ******************************************************************
+      * Author: Felix Eisenmenger
+      * Date:
+      * Purpose: Training
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. minimum.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+
+       PROGRAM-BEGIN.
+            DISPLAY "This program contains four DIVISIONS,".
+            DISPLAY "three PARAGRAPHS".
+            DISPLAY "and four SENTENCES".
+
+       PROGRAM-DONE.
+            STOP RUN.
