f8e0d5de4188dff3dc16932dcc483099ead6deba
Unknown <felixeisenmenger@gmx.net>
2019-06-27 21:26:53 +0200

    [CREATE] day 13 of 21

---
+++ cobshl03.cbl
@@ -0,0 +1,22 @@
+      ******************************************************************
+      * Author: Felix Eisenmenger
+      * Date:
+      * Purpose: training
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. cobshl03.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+
+       PROGRAM-DONE.
+            STOP RUN.
